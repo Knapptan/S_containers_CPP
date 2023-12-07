d9d005ab1ea97e179fafe79f2f6132235b0ac98d
knapptan <nikitas@live.ru>
2023-12-07 20:36:33 +0300

    для гитхаба

---
+++ src/s21_containers.h
@@ -0,0 +1,11 @@
+
+#ifndef S21_CONTAINERS_H_
+#define S21_CONTAINERS_H_
+
+#include <iostream>
+
+#include "list/s21_list.h"
+#include "queue/s21_queue.h"
+#include "set/s21_set.h"
+
+#endif  //S21_CONTAINERS_H_
